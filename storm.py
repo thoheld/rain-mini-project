8703d358374b3f95cc4e4985f244bb39825792b1
thoheld <theld144@gmail.com>
2024-08-10 10:34:42 +0000

    added command line args

--- storm.py
+++ storm.py
@@ -1,9 +1,10 @@
 import curses
 import random
 import time
+import sys
 
-def main(stdscr):
-	
+def main(stdscr):	
+
 	curses.curs_set(0) # hide cursor
 	stdscr.nodelay(1)
 	stdscr.clear() # clear stdscr
@@ -25,11 +26,26 @@ def main(stdscr):
 
 	drops = []
 	
-	new_drop = 7
+	# default settings
+	colorscheme = "blue"
+	speed = 0.075
+	drop_count = width//10
+	# if color given
+	if (len(sys.argv) >= 2):
+		colorscheme = sys.argv[1]
+	# if drop_count given
+	if (len(sys.argv) >= 3):
+		drop_count = int(sys.argv[2])
+	if (len(sys.argv) >= 4):
+		speed = float(sys.argv[3])
+	Drop.colorscheme = colorscheme
+
+	spawn_drop = drop_count//2 # when to spawn new drop
+	new_drop = drop_count//2 # when to spawn new drop
 	while True:
 		
-		if (len(drops) < 16): # max of 16 drops
-			if new_drop == 7:
+		if (len(drops) < drop_count): # max number of drops
+			if new_drop == spawn_drop:
 				drops.append(Drop())
 				new_drop = 0
 			
@@ -42,7 +58,7 @@ def main(stdscr):
 
 		# delay
 		key = stdscr.getch() # quit?
-		time.sleep(.075)
+		time.sleep(speed)
 
 		# move all drops
 		for drop in drops:
@@ -71,6 +87,7 @@ class Drop:
 	
 	screen_width = -1
 	screen_height = -1
+	colorscheme = ""
 
 	# create now drop
 	def __init__(self):
@@ -84,31 +101,33 @@ class Drop:
 	
 	# print drop to stdscr
 	def update_drop(self, stdscr, char_matrix):
-		#colors = [ 15, 45, 44, 39, 39, 38, 38, 38, 33, 33, 33, 27, 27, 27, 27, 27 ]
 		
-		# add new head
-		if (self.y < Drop.screen_height and self.y >= 0): 
-			stdscr.addch(self.y, self.x, char_matrix[self.y][self.x], curses.color_pair(15))
+		if (self.colorscheme == "blue"):
+			#colors = [ 15, 45, 44, 39, 39, 38, 38, 38, 33, 33, 33, 27, 27, 27, 27, 27 ]
+			
+			# add new head
+			if (self.y < Drop.screen_height and self.y >= 0): 
+				stdscr.addch(self.y, self.x, char_matrix[self.y][self.x], curses.color_pair(15))
+			
+			# section 2 (45)
+			if (self.y-1 < Drop.screen_height and self.y-1 >= 0): 
+				stdscr.addch(self.y-1, self.x, char_matrix[self.y-1][self.x], curses.color_pair(45))
+			
+			# section 3
+			if (self.y-3 < Drop.screen_height and self.y-3 >= 0): 
+				stdscr.addch(self.y-3, self.x, char_matrix[self.y-3][self.x], curses.color_pair(33))
+			
+			# section 3
+			if (self.y-7 < Drop.screen_height and self.y-7 >= 0): 
+				stdscr.addch(self.y-7, self.x, char_matrix[self.y-7][self.x], curses.color_pair(27))
 		
-		# section 2 (45)
-		if (self.y-1 < Drop.screen_height and self.y-1 >= 0): 
-			stdscr.addch(self.y-1, self.x, char_matrix[self.y-1][self.x], curses.color_pair(45))
-		
-		# section 3
-		if (self.y-3 < Drop.screen_height and self.y-3 >= 0): 
-			stdscr.addch(self.y-3, self.x, char_matrix[self.y-3][self.x], curses.color_pair(33))
-		
-		# section 3
-		if (self.y-7 < Drop.screen_height and self.y-7 >= 0): 
-			stdscr.addch(self.y-7, self.x, char_matrix[self.y-7][self.x], curses.color_pair(27))
-	
-		# section 4
-		if (self.y-15 < Drop.screen_height and self.y-15 >= 0): 
-			stdscr.addch(self.y-15, self.x, char_matrix[self.y-15][self.x], curses.color_pair(20))
-		
-		# erase end of tail
-		if (self.y-23 < Drop.screen_height and self.y-23 >= 0):
-			stdscr.addch(self.y-23, self.x, char_matrix[self.y-23][self.x], curses.color_pair(16))
+			# section 4
+			if (self.y-15 < Drop.screen_height and self.y-15 >= 0): 
+				stdscr.addch(self.y-15, self.x, char_matrix[self.y-15][self.x], curses.color_pair(20))
+			
+			# erase end of tail
+			if (self.y-23 < Drop.screen_height and self.y-23 >= 0):
+				stdscr.addch(self.y-23, self.x, char_matrix[self.y-23][self.x], curses.color_pair(16))
 		
 
 
